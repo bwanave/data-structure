fee75e14f1c842835197f67f9e691653cb2b4088
Balaji Wanave <52502089+bwanave@users.noreply.github.com>
2022-03-22 21:58:40 +0530

    Update DynamicArrayTest.java

--- src/test/java/ds/DynamicArrayTest.java
+++ src/test/java/ds/DynamicArrayTest.java
@@ -8,9 +8,8 @@ class DynamicArrayTest {
     @Test
     void size() {
         DynamicArray<Integer> dynamicArray = new DynamicArray<>();
-        for (int i = 0; i < 15; i++) {
+        for (int i = 0; i < 15; i++)
             dynamicArray.add(i);
-        }
         Assertions.assertEquals(15, dynamicArray.size());
     }
 }
